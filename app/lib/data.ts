fd22be5d067b9acdcbcb8bd0d4d5bef36c19cca4
tu.tran <tu.tran2@sotatek.com>
2024-12-03 15:58:28 +0700

    finish invoices page : search, render table and pagination on server side

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,11 +15,11 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    // console.log('Data fetch completed after 2 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -84,10 +84,7 @@ export async function fetchCardData() {
 }
 
 const ITEMS_PER_PAGE = 6;
-export async function fetchFilteredInvoices(
-  query: string,
-  currentPage: number,
-) {
+export async function fetchFilteredInvoices(query: string, currentPage: number) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
